bc55e0620c38f941ac13719a6c4c27ee681f7538
Keilla Rocha <keilla.rocha@ba.estudante.senai.br>
2026-06-19 12:36:16 -0300

    thay commit

--- SGB keilla/src/Biblioteca.java
+++ SGB keilla/src/Biblioteca.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+mport java.util.ArrayList;
 
 public class Biblioteca {
 
@@ -14,5 +14,14 @@ public class Biblioteca {
 
     public ArrayList<Livro> getLivros() {
         return livros;
+    }
+
+    public void removerLivro(int codigo) {
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getCodigo() == codigo) {
+                livros.remove(i);
+                break;
+            }
+        }
     }
 }
